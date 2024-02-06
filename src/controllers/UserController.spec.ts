5b0d609dc51878a7619f139655f840a427428efc
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-02-06 10:19:27 -0300

    Update UserController.spec.ts

--- src/controllers/UserController.spec.ts
+++ src/controllers/UserController.spec.ts
@@ -1,5 +1,15 @@
+import { Request, Response } from "express"
 import { UserService } from "../services/UserService"
 import { UserController } from "./UserController"
+import { Params } from "express-serve-static-core"
+
+
+type MockResponse<TResult> = Response & {
+    state: {
+        status?: number,
+        json?: TResult | unknown
+    }
+}
 
 describe('UserController', () =>{
     const mockUserService: Partial<UserService> ={
@@ -7,7 +17,34 @@ describe('UserController', () =>{
     }
     const userController = new UserController(mockUserService as UserService)
 
+    const makeMokeRequest = ({params, query}:{params?: Params, query?: Params}):Request => {
+        const request = {
+            params: params || { },
+            query: query || { }
+        } as unknown
+        return request as Request
+    }
+
+    function makeMokeResponse<TResult> () {
+        const response = {
+            state: {}
+        } as MockResponse<TResult>
+
+        response.status = (status: number) => {
+            response.state.status = status
+            return response
+        }
+
+        response.json = (json: TResult) => {
+            response.state.json = json
+            return response
+        }
+
+        return response
+    }
+
     it('Deve adicionar um novo usuario', () => {
-        console.log(userController)
+        const mockRequest = makeMokeRequest({})
+        const response = userController.createUser(mockRequest)
     })
 })
