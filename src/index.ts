17fdafad1b98110e700d5b7b8981936bbd900e98
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-02-08 10:37:30 -0300

    1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express, { Request, Response } from 'express'
 import { router } from './routes'
+import 'reflect-metadata'
+import { AppDataSource } from "./database"
 
 const server = express()
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been Initialized!")
+    }).catch((err) =>{
+        console.error("Error during Data Source Initilization", err)
+    })
 
 server.use(express.json())
 server.use(router)
--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,6 @@ const userController = new UserController()
 
 router.post('/user', userController.createUser)
 router.get('/user', userController.getAllUsers)
-
 router.delete('/user', (request: Request, response: Response) =>{
     const user = request.body
     console.log(user)
